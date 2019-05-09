230a59ab88e9524701565689a2d6285fe70f2505
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-09 21:52:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.collect do |i| 
+  array.find do |i| 
   cheese_types.include? (i)
 end
 end
